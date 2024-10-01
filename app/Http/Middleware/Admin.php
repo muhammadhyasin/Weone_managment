efb125de4231f75c2fb95bceb13613da7175b5c6
Muhammed Yasin <33571385+muhammadhyasin@users.noreply.github.com>
2024-10-02 02:35:16 +0530

    recent

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check()) {
+            $userRole = Auth::user()->role;
+
+           
+            if ($userRole === 'admin') {
+                return $next($request); 
+            } elseif ($userRole === 'office') {
+                return redirect('office'); 
+            } elseif ($userRole === 'driver') {
+                return redirect('driver'); 
+            }
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,13 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('admin-dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('admin.dashboard');})->middleware(['auth', 'admin'])->name('dashboard');
+    
+Route::get('/office', function () {
+    return view('office.dashboard');})->middleware(['auth', 'verified'])->name('office-dashboard');
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/dashboard', function () {
-    return view('admin-dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+Route::get('/driver', function () {
+    return view('driver.dashboard');})->middleware(['auth', 'verified'])->name('driver-dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -22,9 +19,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 Route::prefix('admin')->group(function () {
-    Route::get('/', [AdminController::class, 'index'])->name('admin.index');
-    Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
-    // Add other admin routes here
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.index');
+    
+    
 });
 
 
