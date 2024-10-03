69fcdd94a701a8a5bd16c3516d2526195f3819e5
Muhammed Yasin <33571385+muhammadhyasin@users.noreply.github.com>
2024-10-03 19:28:36 +0530

    commmit css

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -40,7 +40,7 @@
                 <!-- You can add a logo or any other content here if needed -->
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4   shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
